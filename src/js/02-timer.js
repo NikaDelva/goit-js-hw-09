8a7d968bea69bb59db8c9af6902e55d7f364a388
NikaDelva <nikadelva@gmail.com>
2023-04-06 06:46:09 +0300

    task  2 3 completed

---
+++ src/js/02-timer.js
@@ -0,0 +1,95 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+const startBtnEl = document.querySelector('[data-start]');
+startBtnEl.setAttribute('disabled', true);
+const rootSelector = document.querySelector('.timer');
+
+
+flatpickr("#datetime-picker", {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      window.alert(`Please choose a date in the future!`);
+      startBtnEl.setAttribute('disabled', true);
+    } else {
+      startBtnEl.removeAttribute('disabled', true);
+      timer.deadline = selectedDates[0];
+    }
+  }
+})  
+
+const timer = {
+  intervalId: null,
+  start() {
+    this.intervalId = setInterval(() => {
+      const ms = this.deadline - Date.now();
+      if (ms <= 0) {
+        this.stop();
+        return
+      }
+      let { days, hours, minutes, seconds } = this.convertMs(ms);
+      rootSelector.querySelector('[data-days]').innerHTML =
+        this.addLeadingZero(days);
+      rootSelector.querySelector('[data-hours]').innerHTML =
+        this.addLeadingZero(hours);
+      rootSelector.querySelector('[data-minutes]').innerHTML =
+        this.addLeadingZero(minutes);
+      rootSelector.querySelector('[data-seconds]').innerHTML =
+        this.addLeadingZero(seconds);
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+  },
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  },
+  
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  },
+
+};
+    
+startBtnEl.addEventListener('click', timer.start.bind(timer));
+
+//step 1 ??
+//step 2 викликати функцію flatpickr за документацією (який аргумент їй передати?), що ініціалізує бібліотеку
+// flatpickr створює інтрефейс календаря?
+//step 3 flatpickr(selector, options) -- що таке селектор?
+//options -- подставити об'єкт
+//const options = {
+ // enableTime: true,
+ // time_24hr: true,
+ // defaultDate: new Date(),
+ // minuteIncrement: 1,
+  //onClose(selectedDates) {
+    //console.log(selectedDates[0]);
+  //},
+//};
+
+//step 3 в методі онклоуз написати перевірку дати? які змінні сюди включити? поточний час обраний користувачем? та порівняти його з датою + та - від нього? як правильно написати дату обрану користувачем? писати  через інпут велью? через селектед дейс с індексом?
+//також повісити перемикач активної/неактивної кнопки старт
+//також треба повісити лістенер по кнопці старт / в якій області видимості?
+//відлік часу: це вже створення нової функції? створити нові 3 функції: відліку часу, рендеру та конвертації
+//через що вставляти значення -- _текст контент_, велью чи иннер/інсерт хтмл?
+//
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+const formEl = document.querySelector('.form');
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+  let delay = Number(event.target.elements.delay.value);
+  console.log(delay);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i,  delay)
+    .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay + step;
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  }, delay);
+}
+
+formEl.addEventListener('submit', onFormSubmit);
